9b5ae947a751a993da39d5d3ea5d40bb3b927b20
liamlaubacher <laubacherliam@gmail.com>
2024-10-01 16:25:55 +0200

    Updatet

--- TwoPlayerGame.py
+++ TwoPlayerGame.py
@@ -5,30 +5,32 @@ class Player:
         self.health = health
 
 
-
 def fight(attacker, opponent):
     AoderH = input(F"{attacker.name} willst du angreifen oder heilen ")
     if (AoderH == "angreifen"):
-        schaden = random.randrange(1 ,25 )
+        schaden = random.randrange(10 ,25 )
         opponent.health -= schaden
         print("Es wurden ",schaden,"abgezogen")
         print(F" {opponent.name} hat noch",opponent.health,"leben")
 
+
     if(AoderH =="heilen"):
-        heilen = random.randrange(1,25)
+        heilen = random.randrange(10,25)
         attacker.health+=heilen
         print("Es wurden",heilen,F"geheilt.{attacker.name} hat jetzt",attacker.health," leben")
 
+
 def startGame():
-    Player2=Player(input("Nenne mir deinen Namen Spiler1 "))
+    Player2=Player(input("Nenne mir deinen Namen Spiler1 "),110)
     Player1=Player(input("Was ist dein Namen Spiler2 "))
 
     while(Player1.health>0):
         fight(Player1, Player2)
+
         if (Player2.health<=0):
             print(Player1.name,"hatGewonnen")
             break
-
+        
         fight(Player2, Player1)
         if (Player1.health<=0):
             print(Player2.name,"hatGewonnen")
